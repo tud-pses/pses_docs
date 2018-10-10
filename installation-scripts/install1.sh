2b2f02904ecc0598fe3ffc425ad76d9cc048e904
Nsteel <nicoacero94@gmail.com>
2018-10-10 18:57:23 +0200

    Updated installation scripts

--- installation-scripts/install1.sh
+++ installation-scripts/install1.sh
@@ -7,12 +7,13 @@ then
     exit 1
 fi
 
+# Part 1
 sudo dpkg --configure -a
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install chromium-browser
 sudo apt-get -y install gedit
-sudo add-apt-repository -y ppa:levi-armstrong/qt-libraries-xenial
+sudo add-apt-repository -y ppa:levi-armstrong/qt-libraries-bionic
 sudo add-apt-repository -y ppa:levi-armstrong/ppa
 sudo apt update
 sudo apt-get -y install qt59creator
@@ -21,13 +22,35 @@ sudo apt-get update
 sudo apt-get -y install cutecom
 sudo apt-get -y install git
 sudo apt-get -y install openssh-server
+sudo apt-get -y install at-spi2-core
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get -y install ros-kinetic-desktop-full
-sudo rosdep init
-rosdep update
+sudo apt-get -y install ros-melodic-desktop-full
 sudo apt-get -y install python-rosinstall
 mkdir -p $HOME/catkin_ws/src
 sudo apt-get -y install --reinstall cmake
 sudo usermod -a -G dialout $USER
+# Part 2
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source /opt/ros/melodic/setup.bash
+sudo rosdep init
+rosdep update
+cd $HOME/catkin_ws/src
+catkin_init_workspace
+cd $HOME/catkin_ws/
+catkin_make
+# Part 3
+echo "source $HOME/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source $HOME/catkin_ws/devel/setup.bash
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install libgles2-mesa
+cd $HOME/catkin_ws/src
+rm CMakeLists.txt
+git clone https://github.com/tud-pses/pses_ucbridge.git
+git clone https://github.com/tud-pses/pses_simulation.git
+git clone https://github.com/tud-pses/pses_dashboard.git
+git clone https://github.com/tud-pses/serial.git
+cd ..
+rosdep install -y -r --from-paths .
+catkin_make -DCMAKE_BUILD_TYPE="Release"
